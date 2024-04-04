8937ce464bbb9a869574cbb344d8fbcbc642da20
flowersea <81288375+flowersea520@users.noreply.github.com>
2024-04-05 00:54:35 +0800

    Update globalRequest.ts

--- src/plugins/globalRequest.ts
+++ src/plugins/globalRequest.ts
@@ -12,12 +12,19 @@ import {stringify} from "querystring";
  */
 const request = extend({
   credentials: 'include', // 默认请求是否带上cookie
-  prefix: process.env.NODE_ENV === 'production' ? 'http://user-backend.code-nav.cn' : undefined
+  /**
+   * 关于 'http://user-backend.code-nav.cn' 这个 URL，它通常指的是 后端 服务的地址。在前端开发中，当你需要与后端 API 进行通信时，
+   * 你会使用这个 URL 作为请求的基础地址。在这种情况下，如果 NODE_ENV 是 'production'，则你的应用程序将使用这个地址与后端通信；
+   * 如果 NODE_ENV 不是 'production'（例如，它可能是 'development'），则前缀将是 undefined，你可能需要在其他地方定义或处理这个值。
+   */
+  // 这里通过环境变量： 实现根据不同的环境，实现请求不同的地址 (这里可以写自己的域名，也可以写：“http://公网ip:8080”）
+  prefix: process.env.NODE_ENV === 'production' ? 'http://39.101.78.159:8080' : undefined,
   // requestType: 'form',
 });
 
+
 /**
- * 所以请求拦截器
+ * 所有请求拦截器
  */
 request.interceptors.request.use((url, options): any => {
   console.log(`do request url = ${url}`)
